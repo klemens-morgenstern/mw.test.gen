e209b58ae0cbd4d2aa199b2e5cf19fd141d0cdda
klemens-morgenstern <klemens.morgenstern@gmx.net>
2015-12-30 02:58:53 +0100

    set of rules is complete, though not yet completely tested.

---
+++ include/mw/test/parser/main.hpp
@@ -0,0 +1,45 @@
+/**
+ * @file   id.hpp
+ * @date   14.11.2015
+ * @author Klemens
+ *
+ * Published under [Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0.html)
+ */
+
+#ifndef MW_TEST_PARSER_ID_HPP_
+#define MW_TEST_PARSER_ID_HPP_
+
+
+#include <mw/test/parser/objects.hpp>
+#include <mw/test/parser/group.hpp>
+#include <mw/test/parser/use_file.hpp>
+
+#include <mw/test/ast/main.hpp>
+#include <string>
+
+namespace mw
+{
+namespace test
+{
+namespace parser
+{
+
+
+x3::rule<class main, ast::main> const main;
+
+auto const main_def =
+		*omit[
+			  	test_object[l([](auto &c){_val(c).test_objects.push_back(_attr(c));})]
+			  |	group      [l([](auto &c){_val(c).groups.push_back(_attr(c));})]
+			  | use_file   [l([](auto &c){_val(c).use_files.push_back(_attr(c));})]
+			  ];
+
+
+BOOST_SPIRIT_DEFINE(main);
+
+}
+}
+}
+
+
+#endif /* MW_TEST_PARSER_ID_HPP_ */
