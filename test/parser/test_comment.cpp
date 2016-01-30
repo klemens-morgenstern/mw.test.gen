45b4cefea1112bc312489132292cdbca28dbd999
klemens-morgenstern <klemens.morgenstern@gmx.net>
2016-01-30 17:03:10 +0100

    comment and utitlity work

--- test/parser/test_comment.cpp
+++ test/parser/test_comment.cpp
@@ -89,7 +89,7 @@ int test_main (int, char**)
 
 	/************ Now test documentation rules *****************/
 
-	mw::test::ast::doc_t d;
+	mw::test::data::doc_t d;
 
 	auto p2 = [&](auto rule)
 		{
